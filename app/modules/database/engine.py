9678920c7824054fa0bea694243459ec56d8cf37
TheJecksMan <themarvindit@gmail.com>
2022-09-17 11:19:45 +0300

    🐛 fixed crash db session

--- app/modules/database/engine.py
+++ app/modules/database/engine.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from core.setting import SQLALCHEMY_DATABASE_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_recycle=1800)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
