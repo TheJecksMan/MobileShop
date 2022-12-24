4dcc75a346e04b517f59139646dbeb40cdbd80e7
TheJecksMan <themarvindit@gmail.com>
2022-12-24 16:26:15 +0300

    update

--- app/core/setting.py
+++ app/core/setting.py
@@ -1,4 +1,5 @@
-"""Settings base"""
+"""Settings for Docker"""
+import os
 from pathlib import Path
 from fastapi_mail import ConnectionConfig
 
@@ -6,19 +7,19 @@ TITLE: str = 'Mobile API Service'
 DESCRIPTION: str = 'Mobile API Service - служит сервисом для мобильного приложения. Получение данных от CMS OpenCard.'
 VERSION: str = '1.0.0'
 
-DATABASE_USERNAME: str = "u1346925_test"
-DATABASE_PASSWORD: str = "kV7fK9cJ6t"
-DATABASE_IP: str = "31.31.196.208"
-DATABASE_NAME: str = "u1346925_mebel"
+DATABASE_USERNAME: str = os.environ["database_username"]
+DATABASE_PASSWORD: str = os.environ["database_password"]
+DATABASE_IP: str = os.environ["database_ip"]
+DATABASE_NAME: str = os.environ["database_name"]
 
-SQLALCHEMY_ASYNC_DATABASE_URL: str = f"mysql+asyncmy://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_IP}/{DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL: str = f"mysql+asyncmy://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_IP}/{DATABASE_NAME}"
 
 CONF = ConnectionConfig(
-    MAIL_USERNAME="domaintestsmtp",
-    MAIL_PASSWORD="dN7Q4*WsZTG!RzU",
-    MAIL_FROM="domaintestsmtp@rambler.ru",
-    MAIL_PORT=465,
-    MAIL_SERVER="smtp.rambler.ru",
+    MAIL_USERNAME=os.environ["mail_username"],
+    MAIL_PASSWORD=os.environ["mail_password"],
+    MAIL_FROM=os.environ["mail_from"],
+    MAIL_PORT=os.environ["mail_port"],
+    MAIL_SERVER=os.environ["mail_server"],
     MAIL_SSL=True,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True,
