75bef2c1273368f7c4eeb7c766ca5e5c3000f236
TheJecksMan <themarvindit@gmail.com>
2023-02-11 13:31:36 +0300

    🆕 added async tests

--- app/tests/endpoints/test_product.py
+++ app/tests/endpoints/test_product.py
@@ -1,38 +1,72 @@
 """TestClient"""
-from tests.main_start import client
+from httpx import AsyncClient
+from tests.main_start import BASE_URL, appTest, anyio_backend
 
 
-def test_popular_product():
+async def test_popular_product(anyio_backend):
     """Тестирование получения самых просматриваемых продуктов"""
-    response = client.get("/api/product/popular/5")
-    assert response.status_code == 200
+    async with AsyncClient(app=appTest, base_url=BASE_URL) as client:
+        response_with_params = await client.get("/api/product/popular/5")
+        response_uncorrect_params = await client.get("/api/product/popular/-5")
+
+    assert response_with_params.status_code == 200
+    assert response_uncorrect_params.status_code == 400
 
 
-def test_option_product():
+async def test_option_product(anyio_backend):
     """Получение информации о продуктах"""
-    response = client.get("/api/product/filter/all")
-    assert response.status_code == 200
+    async with AsyncClient(app=appTest, base_url=BASE_URL) as client:
+        response_without_params = await client.get("/api/product/filter/all")
+
+    assert response_without_params.status_code == 200
 
 
-def test_search_product():
+async def test_search_product(anyio_backend):
     """Тестирование поиска товара"""
-    response = client.get("/api/product/search/%D0%BA%D1%80%D0%BE?limit=5")
-    assert response.status_code == 200
+    async with AsyncClient(app=appTest, base_url=BASE_URL) as client:
+        response_with_params = await client.get("/api/product/search/%D0%BA%D1%80%D0%BE?limit=5")
+        response_uncorrect_params = await client.get("/api/product/search/%D0%BA%D1%80%D0%BE?limit=-5")
+
+    assert response_with_params.status_code == 200
+    assert response_uncorrect_params.status_code == 400
 
 
-def test_description_product():
+async def test_description_product(anyio_backend):
     """Тестирование получения описания товара"""
-    response = client.get("/api/product/288/description")
-    assert response.status_code == 200
+    async with AsyncClient(app=appTest, base_url=BASE_URL) as client:
+        response_with_params = await client.get("/api/product/288/description")
+        response_uncorrect_params = await client.get("/api/product/-288/description")
+
+    assert response_with_params.status_code == 200
+    assert response_uncorrect_params.status_code == 404
 
 
-def test_equipment_product():
+async def test_equipment_product(anyio_backend):
     """Тестирование полученнной комплектации товара"""
-    response = client.get("/api/product/equipment/78")
-    assert response.status_code == 200
+    async with AsyncClient(app=appTest, base_url=BASE_URL) as client:
+        response_with_params = await client.get("/api/product/equipment/78")
+        response_uncorrect_params = await client.get("/api/product/equipment/-78")
+
+    assert response_with_params.status_code == 200
+    assert response_uncorrect_params.status_code == 200
+    assert response_uncorrect_params.json() == []
 
 
-def test_product():
+async def test_get_product(anyio_backend):
     """Тестирование получения базовой информации о продукте"""
-    response = client.get("/api/product/78")
-    assert response.status_code == 200
+    async with AsyncClient(app=appTest, base_url=BASE_URL) as client:
+        response_with_params = await client.get("/api/product/78")
+        response_uncorrect_params = await client.get("/api/product/-78")
+
+    assert response_with_params.status_code == 200
+    assert response_uncorrect_params.status_code == 400
+
+
+async def test_product_multiple(anyio_backend):
+    """Тестирование получение информации о нескольких товарах"""
+    async with AsyncClient(app=appTest, base_url=BASE_URL) as client:
+        response_with_params = await client.post("/api/product/multiple", json={"ids": [66, 67]})
+        response_uncorrect_params = await client.post("/api/product/multiple")
+
+    assert response_with_params.status_code == 200
+    assert response_uncorrect_params.status_code == 422
