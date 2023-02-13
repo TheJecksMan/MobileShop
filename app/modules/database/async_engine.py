6773cc68ff82a2896903138675225f29fac965d7
TheJecksMan <themarvindit@gmail.com>
2023-02-13 21:35:53 +0300

    fixed async_sessionmaker

--- app/modules/database/async_engine.py
+++ app/modules/database/async_engine.py
@@ -1,12 +1,11 @@
 """database initialization"""
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from sqlalchemy.ext.asyncio import AsyncSession
 from core.setting import SQLALCHEMY_ASYNC_DATABASE_URL
 
 
 async_engine = create_async_engine(SQLALCHEMY_ASYNC_DATABASE_URL, pool_recycle=1800, pool_pre_ping=True)
-async_session = sessionmaker(
-    async_engine, class_=AsyncSession, expire_on_commit=False
+async_session = async_sessionmaker(
+    async_engine, expire_on_commit=False
 )
